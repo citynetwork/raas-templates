17a5757579a9715c3c7c496d40b9c0f2d6e79da4
Stefano Cucchiella <stefano.cucchiella@citynetwork.eu>
2021-07-06 09:27:40 +0200

    Restores buckets creation

---
+++ clusters/raas-custom/buckets.tf
@@ -0,0 +1,10 @@
+
+// NOTE: ObjectStorage (S3, Swift) is available only in Kna1, Fra1, Tky1, Dx1
+
+resource "openstack_identity_ec2_credential_v3" "ec2_creds" {
+  // By setting it empty, is uses the current project scope
+}
+
+resource "openstack_objectstorage_container_v1" "cluster_etcd_backup" {
+  name = "${var.prefix}-cluster-etcd-backup"
+}
