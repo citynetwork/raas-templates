badbe8db8e4fc542a882440fc30f811011c150b4
Stefano Cucchiella <stefano.cucchiella@citynetwork.eu>
2021-07-06 09:25:15 +0200

    Disables buckets creation

--- clusters/raas-custom/buckets.tf
+++
@@ -1,8 +0,0 @@
-
-resource "openstack_identity_ec2_credential_v3" "ec2_creds" {
-  // By setting it empty, is uses the current project scope
-}
-
-resource "openstack_objectstorage_container_v1" "cluster_etcd_backup" {
-  name = "${var.prefix}-cluster-etcd-backup"
-}
